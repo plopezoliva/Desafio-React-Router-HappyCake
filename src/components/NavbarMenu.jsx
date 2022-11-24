a0444466597ab8a0b2a67828ecc859cf0f71033e
plopezoliva <schrammlopez@gmail.com>
2022-11-24 11:18:02 -0300

    se agrega {Link} a NavbarMenu

--- src/components/NavbarMenu.jsx
+++ src/components/NavbarMenu.jsx
@@ -1,24 +1,22 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
-
 import Navbar from "react-bootstrap/Navbar";
-// import NavDropdown from 'react-bootstrap/NavDropdown';
 import { Link } from "react-router-dom";
 
 const NavbarMenu = () => {
   return (
-     <Navbar bg="danger" variant="dark">
-<Container className="justify-content-start">
-<Link to="/" className="text-white ms-3 text-decoration-none">
-🏠Home
-</Link>
-<Link to="/contacto" className="text-white ms-3 text-decoration-none">
-📒Contacto
-</Link>
-</Container>
-<Navbar.Brand className="me-5">Happy Cake 🍰</Navbar.Brand>
-</Navbar>
-
+    <Navbar bg="danger" variant="dark">
+      <Container className="justify-content-start">
+        <Link as={Link} to="/" className="text-white ms-3 text-decoration-none">
+          🏠Home</Link>
+        <Link
+          as={Link}
+          to="/contacto"
+          className="text-white ms-3 text-decoration-none"
+        >📒Contacto</Link>
+      </Container>
+      <Navbar.Brand className="me-5">Happy Cake 🍰</Navbar.Brand>
+    </Navbar>
   );
 };
 
